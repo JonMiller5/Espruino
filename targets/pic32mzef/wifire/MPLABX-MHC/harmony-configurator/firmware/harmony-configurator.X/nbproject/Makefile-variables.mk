4054d7eba8515fc98584821779f4c60d419bddfe
Jason Kajita <jason.kajita@microchip.com>
2016-10-14 17:34:20 -0700

    Beginning to add experimental support for PIC32 chipKIT Wifire Board. Not ready to use.

---
+++ targets/pic32mzef/wifire/MPLABX-MHC/harmony-configurator/firmware/harmony-configurator.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# chipKIT WiFire configuration
+CND_ARTIFACT_DIR_chipKIT WiFire=dist/chipKIT WiFire/production
+CND_ARTIFACT_NAME_chipKIT WiFire=harmony-configurator.X.production.hex
+CND_ARTIFACT_PATH_chipKIT WiFire=dist/chipKIT WiFire/production/harmony-configurator.X.production.hex
+CND_PACKAGE_DIR_chipKIT WiFire=${CND_DISTDIR}/chipKIT WiFire/package
+CND_PACKAGE_NAME_chipKIT WiFire=harmony-configurator.x.tar
+CND_PACKAGE_PATH_chipKIT WiFire=${CND_DISTDIR}/chipKIT WiFire/package/harmony-configurator.x.tar
